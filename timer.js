85b22006e9ea1c050e410ac0ae03e75c259b2a6e
Nikko Rey <nikkoreyhular0818@gmail.com>
2020-09-23 01:29:08 +0400

    first commit

---
+++ timer.js
@@ -0,0 +1,85 @@
+class Timer {
+  constructor(durationInput, running, startButton, pauseButton, callBacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.running = running;
+
+    if (callBacks) {
+      this.onStart = callBacks.onStart;
+      this.onTick = callBacks.onTick;
+      this.onComplete = callBacks.onComplete;
+    }
+
+    // SetUp EventHandler
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.reset);
+    this.durationInput.addEventListener("click", this.inputChanged);
+  }
+
+  start = () => {
+    if (play.classList.contains("fa-pause")) {
+      this.pause();
+      play.classList.replace("fa-pause", "fa-play");
+      play.setAttribute("title", "Play");
+      this.running = false;
+    } else if (!this.running) {
+      play.classList.replace("fa-play", "fa-pause");
+      play.setAttribute("title", "Pause");
+      if (this.onStart) {
+        this.onStart(this.timeRemaining);
+      }
+      this.tick();
+      this.interval = setInterval(this.tick, 20);
+      this.running = true;
+    }
+  };
+
+  reset = () => {
+    clearInterval(this.interval);
+    this.durationInput.value = "30";
+    play.classList.replace("fa-pause", "fa-play");
+    play.setAttribute("title", "Play");
+    if (this.running || !this.running) {
+      circle.setAttribute("stroke-dashoffset", 0);
+      this.running = false;
+    }
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  inputChanged = () => {
+    this.pause();
+    play.classList.replace("fa-pause", "fa-play");
+    play.setAttribute("title", "Play");
+    this.running = false;
+  };
+
+  tick = () => {
+    //   sample 1
+    // const timeRemaining = parseFloat(this.durationInput.value);
+    // this.durationInput.value = timeRemaining - 1;
+    // Setup Condition
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.02;
+      if (this.onTick) {
+        this.onTick(this.timeRemaining);
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+}
